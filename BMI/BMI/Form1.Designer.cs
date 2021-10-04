0b686800c51852af5c126cbe6f6c16971849a64c
Narliev <91663914+Narliev@users.noreply.github.com>
2021-10-05 02:45:59 +0300

    Add files via upload

---
+++ BMI/BMI/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace BMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.show_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.calc = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.warning = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // show_button
+            // 
+            this.show_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.show_button.Location = new System.Drawing.Point(12, 48);
+            this.show_button.Name = "show_button";
+            this.show_button.Size = new System.Drawing.Size(196, 56);
+            this.show_button.TabIndex = 1;
+            this.show_button.Text = "Are you ready to calculate your BMI?";
+            this.show_button.UseVisualStyleBackColor = true;
+            this.show_button.Click += new System.EventHandler(this.show_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Weight:";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(113, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 36);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Height:";
+            this.label2.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(113, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 36);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 36);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "BMI Calculator";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calc
+            // 
+            this.calc.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calc.Location = new System.Drawing.Point(12, 231);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(95, 36);
+            this.calc.TabIndex = 7;
+            this.calc.Text = "Calculate";
+            this.calc.UseVisualStyleBackColor = true;
+            this.calc.Visible = false;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clear.Location = new System.Drawing.Point(113, 231);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(95, 36);
+            this.clear.TabIndex = 8;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Visible = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(13, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 30);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Result:";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(113, 293);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 30);
+            this.label5.TabIndex = 10;
+            this.label5.Visible = false;
+            // 
+            // warning
+            // 
+            this.warning.AutoSize = true;
+            this.warning.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.warning.Location = new System.Drawing.Point(19, 45);
+            this.warning.Name = "warning";
+            this.warning.Size = new System.Drawing.Size(189, 66);
+            this.warning.TabIndex = 11;
+            this.warning.Text = "You should input both \r\nweight and height\r\nbefore calculating!";
+            this.warning.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.warning.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 349);
+            this.Controls.Add(this.warning);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.calc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.show_button);
+            this.Name = "Form1";
+            this.Text = "BMI Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button show_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label warning;
+    }
+}
+
---
+++ BMI/BMI/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void show_button_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+            label1.Visible = true;
+            textBox2.Visible = true;
+            label2.Visible = true;
+            calc.Visible = true;
+            clear.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            show_button.Visible = false;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calc_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length==0 || textBox2.Text.Length == 0)
+            {
+                warning.Visible = true;
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            else
+            {
+                warning.Visible = false;
+                double num1 = Convert.ToDouble(textBox1.Text);
+                double num2 = Convert.ToDouble(textBox2.Text);
+                label5.Text = ((num1 / (num2 * num2)) * 10000).ToString("N2");
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            label5.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
